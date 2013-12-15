833fd5a7935a4c00e6b3e329931b86042defa9ee
Dai Akatsuka <d.akatsuka@gmail.com>
2013-12-15 20:46:23 +0900

    Add init script

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,13 +12,40 @@ when 'rhel', 'fedora', 'suse'
 end
 
 include_recipe 'build-essential'
-include_recipe "rbenv::default"
-include_recipe "rbenv::ruby_build"
+include_recipe 'rbenv::default'
+include_recipe 'rbenv::ruby_build'
 
-rbenv_ruby "1.9.3-p484" do
-  global true
+group 'mailcatcher' do
+  action :create
+end
+
+user 'mailcatcher' do
+  group 'mailcatcher'
+  home '/var/run/mailcatcher'
+  shell '/bin/false'
+  password nil
+  supports :manage_home => true
+  action [:create, :manage]
+end
+
+rbenv_ruby '1.9.3-p484' do
 end
 
 rbenv_gem 'mailcatcher' do
   ruby_version '1.9.3-p484'
+end
+
+service 'mailcatcher' do
+  supports :start => true, :stop => true, :restart => true
+  action :nothing
+end
+
+template 'mailcatcher' do
+  path '/etc/init.d/mailcatcher'
+  source 'mailcatcher.init.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  notifies :enable, 'service[mailcatcher]'
+  notifies :start, 'service[mailcatcher]'
 end
