c09c3d13f03cf7c936df116952ef981e94e5e476
Dai Akatsuka <d.akatsuka@gmail.com>
2013-12-16 20:01:28 +0900

    Fix typo

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'd.akatsuka@gmail.com'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures mailcatcher'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe           'mailcatcher', 'Install MailCatcher'
 
